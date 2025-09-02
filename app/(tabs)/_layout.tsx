c444f9b77dc2c8f75deda1fdfcde87481f961724
adithyan@12789 <adithiruthiparambil12@gmail.com>
2025-09-02 18:53:38 +0530

    feat: :sparkles: Added Search UI and Fuctionality

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,15 +1,15 @@
 import { Tabs } from "expo-router";
 import { Image, ImageBackground, Text, View } from "react-native";
+import { icons } from "../../constants/icons";
+import { images } from "../../constants/images";
 
-import { icons } from "@/constants/icons";
-import { images } from "@/constants/images";
 
 function TabIcon({ focused, icon, title }: any) {
   if (focused) {
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-16 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
         <Text className="text-secondary text-base font-semibold ml-2">
