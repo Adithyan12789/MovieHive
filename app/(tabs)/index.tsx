085c76230c72fb6ba238218e1d810b171c0ad9ee
adithyan@12789 <adithiruthiparambil12@gmail.com>
2025-09-02 17:30:18 +0530

    feat: :art: Added Modern Navbar

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,10 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
+      <Text className="text-xl font-bold text-primary">
+        Welcome to MovieHive!
       </Text>
+        <Text className="text-red-600">Go to OnBoarding</Text>
     </View>
   );
 }
