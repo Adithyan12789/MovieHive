3f48b3c83e263cb75be011e51c5e61386e0803b6
adithyanNarayanan <adithyan.narayanan@orbitcoders.com>
2025-11-11 16:23:15 +0530

    feat: :sparkles: Created a data fetching hook

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,76 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { Image, ScrollView, View } from "react-native";
+import { ActivityIndicator, FlatList, Image, ScrollView, Text, View } from "react-native";
 import { icons } from "../../constants/icons";
 import { images } from "../../constants/images";
 
 export default function Index() {
-
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
 
   return (
     <View className="flex-1 bg-primary">
-      <Image source={images.bg} className="absolute w-full" />
+      {/* Background Image */}
+      <Image source={images.bg} className="absolute w-full h-full" />
 
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}>
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 20 }}
+      >
+        {/* App Logo */}
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-        <View className="flex-1 mt-5">
-          <SearchBar onPress={() => router.push("/search")} placeholder="Search for a movie..."/>
-        </View>
+        {/* Loader / Error / Data */}
+        {moviesLoading ? (
+          <ActivityIndicator size="large" color="#0000ff" className="mt-10 self-center" />
+        ) : moviesError ? (
+          <Text className="text-red-500 text-center mt-5">
+            Error: {moviesError.message}
+          </Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            {/* Search Bar */}
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie..."
+            />
+
+            {/* Movies Section */}
+            <Text className="mt-5 font-bold text-lg text-white mb-3">
+              Latest Movies
+            </Text>
+
+            <FlatList
+              data={movies}
+              scrollEnabled={false}
+              renderItem={({ item }) => (
+                <MovieCard {...item}/>
+              )}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={3}
+              columnWrapperStyle={{
+                justifyContent: "flex-start",
+                gap: 20,
+                paddingRight: 5,
+                paddingBottom: 10
+              }}
+              className="mt-2 pb-32"
+            />
+          </View>
+        )}
       </ScrollView>
     </View>
   );
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,12 +1,38 @@
-import React from 'react'
-import { Text, View } from 'react-native'
+import MovieCard from "@/components/MovieCard";
+import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
+import React from "react";
+import { FlatList, Image, View } from "react-native";
 
-const search = () => {
+const Search = () => {
+  const {
+    data: movies,
+  } = useFetch(() =>
+    fetchMovies({
+      query: "",
+    })
+  );
+
   return (
-    <View>
-      <Text>search</Text>
-    </View>
-  )
-}
+    <View className="flex-1 bg-primary">
+      <Image
+        source={images.bg}
+        className="flex-1 absolute w-full z-0"
+        resizeMode="cover"
+      />
 
-export default search
+      <FlatList
+        data={movies}
+        renderItem={({ item }) => <MovieCard {...item} />}
+        numColumns={3}
+        columnWrapperStyle={{
+          justifyContent: "center",
+          gap: 16
+        }}
+      />
+    </View>
+  );
+};
+
+export default Search; 
